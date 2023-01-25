7eb8ad4f0a17c651e56eda1955f65e3ae7017793
Hadson Roque <hadson.roque01@git config --global user.email hadson.roque01@gmail.com>
2023-01-25 18:42:31 +0100

    Create function to print KennelsHTML

--- index.php
+++ index.php
@@ -14,13 +14,36 @@ class Products
     }
 
 }
-$products = new Products("cane");
-echo $products->getHTML();
 class Kennels extends Products
 {
     public $materials;
     public $size;
     public $price;
+
+    public function __construct($animal, $materials, $size, $price)
+    {
+
+        parent::__construct($animal);
+
+        $this->materials = $materials;
+        $this->size = $size;
+        $this->price = $price;
+
+    }
+
+    public function getKennelsHTML()
+    {
+        return parent::getHTML()
+            . $this->getKennels();
+    }
+
+    public function getKennels()
+    {
+        return "<h3>Materiale: " . $this->materials . "<br>"
+            . "Taglia: " . $this->size . "<br>"
+            . "Prezzo: " . $this->price . "</h3>";
+    }
+
 }
 
 class Foods extends Products
@@ -37,5 +60,9 @@ class Games extends Products
     public $tennisball;
     public $scratchingpost;
 }
+
+// Kennels print in html
+$kennels = new Kennels("Cane", "Plastica", "XXL", "120 EUR");
+echo $kennels->getKennelsHTML();
 
 ?>
